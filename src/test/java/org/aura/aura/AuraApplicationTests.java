806f04257f052bf52f3ddccc1f9e35f718bd488f
kov08 <krushn01@gmail.com>
2026-06-23 11:56:57 -0400

    chore(setup): scaffold bootable Spring Boot skeleton

---
+++ src/test/java/org/aura/aura/AuraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aura.aura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
